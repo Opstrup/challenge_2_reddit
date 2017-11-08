ad8df590fd7f028526acd309d5e78d8713b9b9d7
Anders Opstrup <anders@coffeecode.dk>
2017-11-08 10:17:12 +0100

    finished exercise 1

--- largest_voc_mapreduce.py
+++ largest_voc_mapreduce.py
@@ -4,12 +4,18 @@ import json
 
 class LargestVocabulary(MRJob):
 
-    def mapper_node_connections(self, _, line):
+    def mapper_get_unique_words(self, _, line):
+        symbols = ['\n','`','~','!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','[',']','}','|','\\',':',';','"',"'",'<','>','.','?','/',',']
         json_string = json.loads(line)
-        unique_count = len(set(json_string['body'].split()))
+        lower_string = json_string['body'].lower()
+
+        for sym in symbols:
+            lower_string = lower_string.replace(sym, "")
+
+        unique_count = len(set(lower_string.split()))
         yield json_string['subreddit_id'], unique_count
 
-    def reducer_sum_connections(self, subr_id, unique_counts):
+    def reducer_sum_unique_words(self, subr_id, unique_counts):
         yield None, (subr_id, sum(unique_counts))
 
     def reducer_get_highest_ten(self, _, unique_count):
@@ -18,8 +24,8 @@ class LargestVocabulary(MRJob):
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_node_connections,
-                   reducer=self.reducer_sum_connections),
+            MRStep(mapper=self.mapper_get_unique_words,
+                   reducer=self.reducer_sum_unique_words),
             MRStep(reducer=self.reducer_get_highest_ten)
         ]
 
