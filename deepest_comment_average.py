5542062315fbddac8c1ab2acb8bb8489a3c68604
Anders Opstrup <anders@coffeecode.dk>
2017-11-08 11:30:03 +0100

    started on exercise 3

---
+++ deepest_comment_average.py
@@ -0,0 +1,25 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import json
+
+class LargestVocabulary(MRJob):
+
+    def mapper_get_unique_words(self, _, line):
+        yield json_string['parent_id'], json_string['parent_id']
+
+    def reducer_sum_unique_words(self, subr_id, unique_counts):
+        yield None, (subr_id, sum(unique_counts))
+
+    def reducer_get_highest_ten(self, _, unique_count):
+        sorted_list = sorted(unique_count, key=lambda count: count[1], reverse = True)
+        yield "10 biggest vocabulary", sorted_list[:10]
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_unique_words,
+                   reducer=self.reducer_sum_unique_words),
+            MRStep(reducer=self.reducer_get_highest_ten)
+        ]
+
+if __name__ == '__main__':
+    LargestVocabulary.run()
