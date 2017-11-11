4a8beb9202d6c24e84a614d4edfe53860cc07e6d
Anders Opstrup <anders@coffeecode.dk>
2017-11-11 23:43:48 +0100

    running some experiments on exercise 3

--- deepest_comment_average.py
+++ deepest_comment_average.py
@@ -1,15 +1,25 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 import json
+import itertools
+import operator
 
 class LargestVocabulary(MRJob):
 
     def mapper_get_unique_words(self, _, line):
         json_string = json.loads(line)
-        yield json_string['subreddit_id'], (json_string['parent_id'], json_string['name'])
+        yield (json_string['subreddit_id'], json_string['subreddit']), json_string['parent_id']
 
-    def reducer_sum_unique_words(self, subr_id, unique_counts):
-        yield None, (subr_id, sum(unique_counts))
+    def combiner_grp_all_comments(self, subr, value):
+        yield (subr, value)
+
+    def mapper_yield_funny_subreddits(self, subr, value):
+        (subr_id, subr_name) = subr
+        if subr_id == 't5_2qh33':
+            yield subr, value
+
+    def reducer_sum_unique_words(self, subr, unique_counts):
+        yield None, (subr, sum(unique_counts))
 
     def reducer_get_highest_ten(self, _, unique_count):
         sorted_list = sorted(unique_count, key=lambda count: count[1], reverse = True)
@@ -17,7 +27,9 @@ class LargestVocabulary(MRJob):
 
     def steps(self):
         return [
-            MRStep(mapper=self.mapper_get_unique_words)
+            MRStep(mapper=self.mapper_get_unique_words,
+                   combiner=self.combiner_grp_all_comments)
+            # MRStep(mapper=self.mapper_yield_funny_subreddits)
                 #    reducer=self.reducer_sum_unique_words),
             # MRStep(reducer=self.reducer_get_highest_ten)
         ]
