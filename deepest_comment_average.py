9e0e6b26038045184c4eab650604eb97747bd980
Anders Opstrup <anders@coffeecode.dk>
2017-11-14 10:20:51 +0100

    finished exercise 3

--- deepest_comment_average.py
+++ deepest_comment_average.py
@@ -10,11 +10,6 @@ class LargestVocabulary(MRJob):
         json_string = json.loads(line)
         yield (json_string['subreddit_id'], json_string['subreddit']), json_string['parent_id']
 
-    def mapper_yield_funny_subreddits(self, subr, value):
-        (subr_id, subr_name) = subr
-        if subr_id == 't5_2qh33':
-            yield subr, value
-
     def mapper_count_number_of_comments(self, subr, value):
         if value[:2] == 't3':
             yield subr, 0
@@ -28,28 +23,20 @@ class LargestVocabulary(MRJob):
         else:
             yield subr, sum(list_values) / list_values.count(0)
 
-    def reducer_get_highest_ten(self, subr, average_depth):
-        res_list = []
-        if len(res_list) < 10:
-            res_list.append(average_depth)
-            sorted(res_list, reverse = True)
-            yield subr, sorted(res_list, reverse = True)
-        elif res_list[9] < average_depth:
-            print('res list', res_list)
-            res_list[9] = average_depth
-            yield subr, sorted(res_list, reverse = True)
-        
+    def mapper_get_highest_ten(self, subr, average_depth):
+        yield None, (subr, average_depth)
 
-        # yield "10 highest average depth", res_list
-        # sorted_list = sorted(list(average_depth), reverse = True)
-        # yield "10 highest average depth", sorted_list[:10]
+    def reducer_get_highest_ten(self, _, subr_depth):
+        sorted_list = sorted(subr_depth, key=lambda tup: tup[1], reverse = True)
+        yield "Highest ten: ", sorted_list[:10]
 
     def steps(self):
         return [
             MRStep(mapper=self.mapper_get_data),
             MRStep(mapper=self.mapper_count_number_of_comments,
                    reducer=self.reducer_calculate_average_depth),
-            MRStep(mapper=self.reducer_get_highest_ten)
+            MRStep(mapper=self.mapper_get_highest_ten,
+                   reducer=self.reducer_get_highest_ten)
         ]
 
 if __name__ == '__main__':
